fea33b868ce09123b6bbdb5ff0578b3408b950bb
jaspen <jasper.norring@gmail.com>
2024-02-12 18:06:13 +0200

    Tehtävä tehty chuck norris vitsit

---
+++ Viteserver/vite-project/counter.js
@@ -0,0 +1,11 @@
+export function setupCounter(element) {
+
+
+ 
+  const setCounter = (count) => {
+    console.log("olen täällä")
+
+  }
+  element.addEventListener('click', () => setCounter())
+
+}
